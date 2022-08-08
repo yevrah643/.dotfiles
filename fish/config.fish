7f1101e93ccfd8f41ae7bf4dcbd1c8776c050ebc
harveyPhung <tuongquang.phung@outlook.com>
2022-08-08 23:36:46 +1000

    Fish::remove::tide removed tide from Fish shell

--- fish/config.fish
+++ fish/config.fish
@@ -1,26 +1,3 @@
-set fish_greeting "Nice Day, Harvey!!!"
-
-if type -q exa 
-	alias ll "exa -l -g --icons"
-	alias lla "ll -a"
-end 
-
-# set $VARIABLE 
-set -gx EDITOR nvim 
-set -gx HOME /Users/tuongquangphung/
-
-set -gx PATH bin $PATH
-set -gx PATH ~/.local/bin $PATH
-
-# alias
-alias vim nvim 
-alias g git 
-alias ls "ls -p -G"
-alias la "ls -A"
-alias lla "ll -A"
-
-# >>> conda initialize >>>
-# !! Contents within this block are managed by 'conda init' !!
-eval /Users/tuongquangphung/opt/anaconda3/bin/conda "shell.fish" "hook" $argv | source
-# <<< conda initialize <<<
-
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
