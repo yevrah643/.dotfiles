124c505787add0517c8dadfdc3dad78e0961ffb5
harveyPhung <tuongquang.phung@outlook.com>
2022-08-13 17:41:55 +1000

    org::up::int:: new organised plugins

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,32 +1,3 @@
-require "tpq16.options"
-require "tpq16.keymaps"
-
-require "tpq16.plugins"
-require "tpq16.webdev-icon"
-require "tpq16.autocommands"
-require "tpq16.colorscheme"
-
-require "tpq16.lsp"
-require "tpq16.dap"
-require "tpq16.cmp"
-
-require "tpq16.telescope"
-require "tpq16.treesitter"
-
-require "tpq16.autopairs"
-require "tpq16.comment"
-require "tpq16.gitsigns"
-
-require "tpq16.nvim-tree"
-require "tpq16.bufferline"
-require "tpq16.lualine"
-require "tpq16.toggleterm"
-
-require "tpq16.project"
-require "tpq16.impatient"
-require "tpq16.illuminate"
-require "tpq16.indentline"
-require "tpq16.alpha"
-
---require "tpq16.navic"
-require "tpq16.sniplua"
+require "options"
+require "keymaps"
+require "plugins"
