40c8c0e0139f03cea7cb7e6e4626d831c783570c
harveyPhung <tuongquang.phung@outlook.com>
2022-08-17 00:17:33 +1000

    config:: modify structure

--- nvim/lua/tpq16/lualine.lua
+++ nvim/lua/tpq16/lualine.lua
@@ -5,29 +5,42 @@ end
 
 lualine.setup {
   options = {
-    globalstatus = true,
     icons_enabled = true,
-    theme = "auto",
-    component_separators = { left = "", right = "" },
-    section_separators = { left = " ", right = "" },
+    theme = 'tokyonight',
+    section_separators = { left = '', right = '' },
+    component_separators = { left = '', right = '' },
     disabled_filetypes = { "alpha", "dashboard" },
-    always_divide_middle = true,
+    always_divide_middle = true
   },
   sections = {
-    lualine_a = { "mode" },
-    lualine_b = { "branch", "diff"},
-    lualine_c = { "diagnostics" },
-    lualine_x = { "filename", "encoding", "fileformat", "filetype" },
-    lualine_y = { "progress", "spaces"},
-    lualine_z = { "location" },
+    lualine_a = { 'mode' },
+    lualine_b = { 'branch' },
+    lualine_c = { {
+      'filename',
+      file_status = true, -- displays file status (readonly status, modified status)
+      path = 1 -- 0 = just filename, 1 = relative path, 2 = absolute path
+    } },
+    lualine_x = {
+      { 'diagnostics', sources = { "nvim_diagnostic" }, symbols = { error = ' ', warn = ' ', info = ' ',
+        hint = ' ' } },
+      'encoding',
+      'filetype'
+    },
+    lualine_y = { 'progress' },
+    lualine_z = { 'location' }
   },
-
   inactive_sections = {
     lualine_a = {},
     lualine_b = {},
-    lualine_c = { "filename" },
-    lualine_x = { "location" },
+    lualine_c = { {
+      'filename',
+      file_status = true, -- displays file status (readonly status, modified status)
+      path = 2 -- 0 = just filename, 1 = relative path, 2 = absolute path
+    } },
+    lualine_x = { 'location' },
     lualine_y = {},
-    lualine_z = {},
+    lualine_z = {}
   },
+  tabline = {},
+  --extensions = { 'fugitive' }
 }
